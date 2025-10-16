420476b48c38997d1e5f95fe7ac4591864a726f9
Aldwin Seboguero <aldwin.seboguero@parsu.edu.ph>
2025-10-16 13:02:09 +0800

    From anchor to button logout

--- resources/js/Layouts/MyLayout.vue
+++ resources/js/Layouts/MyLayout.vue
@@ -170,14 +170,14 @@
              </li>
              
              <li>
-                <a href="#" @click="logout" class="inline-flex items-center px-4 py-3  text-gray-800  text-md font-black font-sans rounded-lg active w-full dark:bg-blue-600 hover:bg-gray-100 dark:hover:bg-gray-700 group" aria-current="page">
+                <button   @click="logout" class="inline-flex items-center px-4 py-3  text-gray-800  text-md font-black font-sans rounded-lg active w-full dark:bg-blue-600 hover:bg-gray-100 dark:hover:bg-gray-700 group" aria-current="page">
                      
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 me-2  text-gray-800">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
                     </svg>
     
                     Logout
-                </a>
+                </button>
              </li>
           </ul>
        </div>
