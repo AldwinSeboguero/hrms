c3c238d7c67749cd502a91fbb4b8d24167abf134
Aldwin Seboguero <aldwin.seboguero@parsu.edu.ph>
2025-11-20 10:22:16 +0800

    Revise IPCR

--- resources/js/Pages/PM/IPCR.vue
+++ resources/js/Pages/PM/IPCR.vue
@@ -317,19 +317,20 @@ onMounted(() => {
 
 
                     </div>
-                    <div class="col-span-6 sm:col-span-6">
-                        <label for="category"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Year</label>
-                        <input v-model="form.year" required type="number"
-                            class=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-
-                    </div>
+                 
                     <div class="col-span-6 sm:col-span-6">
                         <label for="category"
                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Numerical
                             Rating</label>
                         <input v-model="form.numerical_rating" required type="number" min="0" max="5" step="0.1"
                             @input="checkMaxValue"
+                            class=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+
+                    </div>
+                    <div class="col-span-6 sm:col-span-6">
+                        <label for="category"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Year</label>
+                        <input v-model="form.year" required type="number"
                             class=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
 
                     </div>
